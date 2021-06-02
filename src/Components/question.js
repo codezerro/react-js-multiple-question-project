5c6cc3aeef31cc5f659e179a6dddb3cb4d94f6db
Md. Shariar Kabir <kabircp08@gmail.com>
2021-06-02 22:02:30 +0600

    update npm package

--- src/Components/question.js
+++ src/Components/question.js
@@ -1,41 +1,40 @@
 import React from 'react';
 import QuestinList from './questionList.js';
 
-const Question =(props) => {    
-    
-    const style={
-        minHeight: '90px'
-    }
-    if(props.qIsAnswer&&props.qIsRight){
-        style.backgroundColor='green';
-    }else if(props.qIsAnswer && !props.qIsRight){
-        style.backgroundColor='red';
-    }
-  return (
-    <div>
-        <ul className="list-group">
-            <li 
-                className="list-group-item" 
-                style={style}
-                onClick={props.openDetails}
-            >{props.qId}{' . ' }{props.qText}</li>
-            <hr></hr>
-            <div className="answer">
-                {props.qChoices.map(q=>{
-                    return <QuestinList
-                                key={q}
-                                List={q}
-                                chackChanged={props.chackChanged}
-                                qId={props.qId}
-                                qIsAnswer={props.qIsAnswer}
-                            />
-                })}
-            </div>
-        </ul>
-    </div>
-  )
-}
+const Question = props => {
+	const style = {
+		minHeight: '90px',
+	};
+	if (props.qIsAnswer && props.qIsRight) {
+		style.backgroundColor = 'green';
+	} else if (props.qIsAnswer && !props.qIsRight) {
+		style.backgroundColor = 'red';
+	}
+	return (
+		<div>
+			<ul className='list-group'>
+				<li className='list-group-item' style={style} onClick={props.openDetails}>
+					{props.qId}
+					{' . '}
+					{props.qText}
+				</li>
+				<hr></hr>
+				<div className='answer'>
+					{props.qChoices.map(q => {
+						return (
+							<QuestinList
+								key={q}
+								List={q}
+								chackChanged={props.chackChanged}
+								qId={props.qId}
+								qIsAnswer={props.qIsAnswer}
+							/>
+						);
+					})}
+				</div>
+			</ul>
+		</div>
+	);
+};
 
 export default Question;
-
-
--- src/Components/questionList.js
+++ src/Components/questionList.js
@@ -1,22 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const QuestinList= (props) => { 
-    //console.log(props.qIsAnswer);
-    if(props.qIsAnswer){
-        
-    }
-    return (
-    <div>
-    <ul className="float-left">
-        <li> 
-            <input type="checkbox"
-                value={props.List} 
-                onChange={props.chackChanged}
-            />
-            <span>{props.List}</span>
-        </li>
-    </ul>
-    </div>
-  )
-}
+const QuestinList = props => {
+	//console.log(props.qIsAnswer);
+	if (props.qIsAnswer) {
+	}
+	return (
+		<div>
+			<ul className='float-left'>
+				<li>
+					<input type='checkbox' value={props.List} onChange={props.chackChanged} />
+					<span>{props.List}</span>
+				</li>
+			</ul>
+		</div>
+	);
+};
 export default QuestinList;
